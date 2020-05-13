12956e09bb0d28b45accb5537ad0c046dc4e77f7
laodano1 <281296869@qq.com>
2020-05-13 21:52:53 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+# firefoxdriver = webdriver.Firefox()
+# firefoxdriver.get("https://www.bing.com")
+
+chromeDriver = webdriver.Chrome()
+chromeDriver.get("https://www.bing.com")
+
